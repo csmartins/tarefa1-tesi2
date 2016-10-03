abd572c49a5b6cdbb01fc06faa5546e43386c1f9
Carlos Eduardo da Silva Martins <cmartins@mordor.mordor>
2016-10-03 12:33:10 -0300

    Mais um pouco de tratamento de texto

--- tarefa1.py
+++ tarefa1.py
@@ -5,28 +5,37 @@ import sys
 from nltk.tree import Tree
 import csv
 
+names_dict = {}
+names_dict["Jon Snow"] = ["Jon", "Jon Snow", "Lord Commander Jon Snow", "Lord Snow"]
+names_dict["Benjen Stark"] = ["Benjen Stark", "Benjen", "Uncle Benjen"]
+names_dict["Eddard Stark"] = ["Ned Stark", "Ned", "Lord Eddard", "Eddard Stark", "Eddard", "Lord Eddard Stark"]
+names_dict["Daenerys Targaryen"] = ["Daenerys Stormborn", "Daenarys", "Daenerys Targaryen", "Queen Daenerys Targaryen", "Princess Daenerys Targaryen", "Queen Daenerys", "Daenarys", "Dragon Queen", "Mhysa"]
+
+def clear_entity(entity):
+    return entity.strip().strip("\"").strip("(").strip(")").strip(".").strip("-").strip("[").strip("]").strip("{").strip("}").strip()
+
 def generate_named_entity(s):
     sentences = nltk.sent_tokenize(s.decode('utf-8'))
 
     named_entities = []
     for sentence in sentences:
-	words_tokenized = nltk.word_tokenize(sentence)
+    	words_tokenized = nltk.word_tokenize(sentence)
 
-	pos = nltk.pos_tag(words_tokenized)
-	grammar = ''' 	NE:	{<NNP|NNPS>+}
-				{<NNP|NNPS><POS><NNP|NNPS>}
-				{<NNP|NNPS>+<IN><NNP|NNPS>+}
-		  '''
- 
-	#ner = nltk.ne_chunk(pos, binary=True)
-	regex_parser = nltk.RegexpParser(grammar)
-	ner = regex_parser.parse(pos)
-	    
-	for t in ner:
-		if isinstance(t, nltk.tree.Tree):
-			if t.node == 'NE':
-				text = ' '.join([c[0] for c in t])
-				named_entities.append(text)
+    	pos = nltk.pos_tag(words_tokenized)
+    	grammar = ''' 	NE:	{<NNP|NNPS>+}
+    				{<NNP|NNPS><POS><NNP|NNPS>}
+    				{<NNP|NNPS>+<IN><NNP|NNPS>+}
+    		  '''
+
+    	#ner = nltk.ne_chunk(pos, binary=True)
+    	regex_parser = nltk.RegexpParser(grammar)
+    	ner = regex_parser.parse(pos)
+
+    	for t in ner:
+    		if isinstance(t, nltk.tree.Tree):
+    			if t.label() == 'NE':
+    				text = ' '.join([c[0] for c in t])
+    				named_entities.append(clear_entity(text))
     return named_entities
 
 def remove_empty(content):
@@ -52,14 +61,14 @@ def write_full_content(content, path):
     f.write("\n")
     f.write("\n")
     f.write("\n")
-    
+
     for elem in content:
         f.write(elem+" ")
 
     f.write("\n")
     f.write("\n")
     f.write("\n")
-    
+
 
 def write_named_entities(content, path):
     f = open(path+"_named-entities.txt", 'a')
@@ -71,14 +80,15 @@ def write_named_entities(content, path):
     f.write(str(content))
 
 def list_named_entities(content):
-	named_entities_repetition = []
-        for line in content:
-		line_named_entities = generate_named_entity(line)
-		if line_named_entities != []:
-			for entity in line_named_entities:
-				named_entities_repetition.append(entity.strip())
-	named_entities = list(set(named_entities_repetition))
-	return named_entities
+    named_entities_repetition = []
+    for line in content:
+        line_named_entities = generate_named_entity(line)
+        if line_named_entities != []:
+            for entity in line_named_entities:
+                named_entities_repetition.append(entity.strip())
+
+    named_entities = list(set(named_entities_repetition))
+    return named_entities
 
 def write_named_entities_in_csv(entities, path_episodes):
     entities = map(lambda x: x.encode('utf8'), entities)
@@ -87,7 +97,7 @@ def write_named_entities_in_csv(entities, path_episodes):
     with open(path_episodes+'/output/entities.csv', 'wb') as csvfile:
         spamwriter = csv.writer(csvfile)
 
-	for entity in entities:
+        for entity in entities:
             spamwriter.writerow([entity])
 
 def clean_text(text_by_line):
@@ -107,7 +117,7 @@ def do_main():
     if len(sys.argv) < 2:
         print "Insuficient number of arguments."
         print "Expected: python tarefa1.py <path_to_episodes>"
-	print "Path Example: episodes (if it's in the same directory of this file.)"
+        print "Path Example: episodes (if it's in the same directory of this file.)"
         return
 
     full_content = ""
@@ -117,25 +127,25 @@ def do_main():
     entities = []
     seasons = os.listdir(path_episodes)
     for season in seasons:
-	if "season" in season:
-	    print season
-	    path_season = path_episodes+'/'+season+'/'
-    	    files = os.listdir(path_season)
+    	if "season" in season:
+    	    print season
+            path_season = path_episodes+'/'+season+'/'
+            files = os.listdir(path_season)
             for episode in files:
-		print episode
-        	with open(path_season + episode) as f:
-		     full_content = f.read().splitlines()
-		     full_content = clean_text(full_content)
+                print episode
+                with open(path_season + episode) as f:
+                    full_content = f.read().splitlines()
+                    full_content = clean_text(full_content)
 
-            	     write_full_content(full_content, path_season+"../output/"+season+"/"+episode)
+                    write_full_content(full_content, path_season+"../output/"+season+"/"+episode)
 
-	             write_list_of_line_contents(full_content, path_season+"../output/"+season+"/"+episode)
-		     
-	             entities = list_named_entities(full_content)
-                     write_named_entities(entities, path_season+"../output/"+season+"/"+episode)
-            	     named_entities += entities
+                    write_list_of_line_contents(full_content, path_season+"../output/"+season+"/"+episode)
+
+                    entities = list_named_entities(full_content)
+                    write_named_entities(entities, path_season+"../output/"+season+"/"+episode)
+                    named_entities += entities
     write_named_entities_in_csv(named_entities, path_episodes)
-    
+
 
 if __name__ == "__main__":
     do_main()
