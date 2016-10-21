b032689ceb0c731e48d2046bc00e5fcd706bc7b2
pghiraldelli <patriciaghiraldelli@gmail.com>
2016-10-21 17:51:57 -0200

    Fazendo com que os textos limpos fiquem no formato do texto original e usando regex para limpa-los

--- clean_text.py
+++ clean_text.py
@@ -7,21 +7,25 @@ path_episodes_cleaned = "cleaned_episodes"
 def create_diretory(path):
     if not (os.path.isdir(path)):
 	os.mkdir(path)
+
 def remove_empty(content):
     content = filter(None, content)
     return content
 
-def clean_text(text_by_line):
+def clean_text(text):
 
-	cleaned_text = []
-	for line in text_by_line:
-		#Condicoes para retirar parte debaixo do texto
-		if ("Recap" in line or "Appearances" in line) and len(line) < 20: break
-		#condicao para nao adicionar parte de cima do texto
-		if len(line) > 150: cleaned_text.append(line)
+	init = re.search(r"Plot[ ]?(Edit)?\n", full_content).start() #comeca no plot ou plot edit
+	end = re.search(r"(Recap[ ]?(Edit)?\n|Appearances[ ]?(Edit)?\n)", text).start() #termina no recap ou recap edit ou appearances edit
+	
+	text = text[init:end]
 
-	cleaned_text = remove_empty(cleaned_text)
-	return cleaned_text
+	#remove os plot edit e summary edit do meio do texto
+	text = text.replace("PlotEdit", "").replace("Plot Edit", "").replace("SummaryEdit", "").replace("Summary Edit", "") 
+	#remove os Edit de inicio de descricao de cena
+	text = re.sub(r"([A-Z][a-z]*[ ]?)Edit", r'\1', text) 
+	#remove linhas vazias
+	text = remove_empty(text)
+	return text
 
 def do_main():
 	if len(sys.argv) < 2:
@@ -30,7 +34,6 @@ def do_main():
 		print "Path Example: episodes (if it's in the same directory of this file.)"
 		return
 
-	full_content = ""
 	path_episodes = sys.argv[1]
 	seasons = os.listdir(path_episodes)
 
@@ -43,12 +46,14 @@ def do_main():
 
 			path_season_cleaned = path_episodes_cleaned+'/'+season+'/'
 			create_diretory(path_season_cleaned)
+
 			for episode in files:
+				global full_content
 				with open(path_season + episode) as f:
-					full_content = f.read().splitlines()
-					full_content = clean_text(full_content)
-					with open(path_season_cleaned + episode, 'w') as f:
-						f.write(str(full_content))
+					full_content = f.read()
+					full_content = clean_text(full_content) 	
+				with open(path_season_cleaned + episode, 'w') as f:
+					f.write(str(full_content))
 
 	
 
